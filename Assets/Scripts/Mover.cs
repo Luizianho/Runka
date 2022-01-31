a6b28c808e568ac72806ffb930d6b544bfcff7d3
LuizGaming <45321324+Luizianho@users.noreply.github.com>
2022-01-31 04:17:29 +0100

    Added simple raycasting

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -6,10 +6,15 @@ using UnityEngine.AI;
 public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
+    Ray lastRay;
 
-    // Update is called once per frame
     void Update()
     {
+        if(Input.GetMouseButton(0))
+        {
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
         GetComponent<NavMeshAgent>().destination = target.position;
     }
 }
