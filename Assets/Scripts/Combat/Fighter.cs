e76418597e5c6291134a180872b018a6920ca2cf
LuizGaming <45321324+Luizianho@users.noreply.github.com>
2022-02-02 19:53:47 +0100

    Added time between attack (delay)

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -7,11 +7,14 @@ namespace RPG.Combat
     public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeBetweenAttacks = 1f;
 
         Transform target;
+        float timeSinceLastAttack = 0f;
 
         private void Update()
         {
+            timeSinceLastAttack += Time.deltaTime;
             if(target == null) return;
 
             if (!GetIsInRange())
@@ -21,9 +24,18 @@ namespace RPG.Combat
             else
             {
                 GetComponent<Mover>().Cancel();
-                GetComponent<Animator>().SetTrigger("attack");
+                AttackBehaviour();
             }
+        }
 
+        private void AttackBehaviour()
+        {
+            if (timeSinceLastAttack > timeBetweenAttacks)
+            {
+                GetComponent<Animator>().SetTrigger("attack");
+                timeSinceLastAttack = 0;
+            }
+            
         }
 
         private bool GetIsInRange()
